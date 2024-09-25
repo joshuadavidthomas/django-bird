1c6f44fefbf15bed993d6f66b4aa92f3e1cd9e51
Josh <josh@joshthomas.dev>
2024-09-25 09:48:14 -0500

    fix name

--- src/django_bird/loader.py
+++ src/django_bird/loader.py
@@ -9,7 +9,7 @@ from django.template.engine import Engine
 from django.template.loaders.filesystem import Loader as FileSystemLoader
 
 from ._typing import override
-from .compiler import BirdCompiler
+from .compiler import Compiler
 
 CUSTOM_ELEMENT_RE = re.compile(r"<bird:[^>]+>")
 
@@ -17,7 +17,7 @@ CUSTOM_ELEMENT_RE = re.compile(r"<bird:[^>]+>")
 class BirdLoader(FileSystemLoader):
     def __init__(self, engine: Engine):
         super().__init__(engine)
-        self.compiler = BirdCompiler()
+        self.compiler = Compiler()
 
     @override
     def get_contents(self, origin: Origin) -> str:
