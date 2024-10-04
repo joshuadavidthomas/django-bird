ea26b65b509b8d9a274a8f46d4a06c514880ff60
Josh <josh@joshthomas.dev>
2024-10-03 19:00:53 -0500

    add ability to autoconfigure application in template settings

--- src/django_bird/conf.py
+++ src/django_bird/conf.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from contextlib import suppress
 from dataclasses import dataclass
+from dataclasses import field
+from typing import Any
 
+import django.template
 from django.conf import settings
 
 from ._typing import override
@@ -9,12 +13,78 @@ from ._typing import override
 DJANGO_BIRD_SETTINGS_NAME = "DJANGO_BIRD"
 
 
-@dataclass(frozen=True)
+@dataclass
 class AppSettings:
+    ENABLE_AUTO_CONFIG: bool = True
+    _template_configurator: TemplateConfigurator = field(init=False)
+
+    def __post_init__(self):
+        self._template_configurator = TemplateConfigurator(self)
+
     @override
     def __getattribute__(self, __name: str) -> object:
         user_settings = getattr(settings, DJANGO_BIRD_SETTINGS_NAME, {})
         return user_settings.get(__name, super().__getattribute__(__name))  # pyright: ignore[reportAny]
+
+    def autoconfigure(self) -> None:
+        self._template_configurator.autoconfigure()
+
+
+class TemplateConfigurator:
+    def __init__(self, app_settings: AppSettings, engine_name: str = "django") -> None:
+        self.app_settings = app_settings
+        self.engine_name = engine_name
+        self._configured = False
+
+    def autoconfigure(self) -> None:
+        if not self.app_settings.ENABLE_AUTO_CONFIG:
+            return
+
+        for template_config in settings.TEMPLATES:
+            engine_name = (
+                template_config.get("NAME") or template_config["BACKEND"].split(".")[-2]  # type: ignore[attr-defined]
+            )
+            if engine_name != self.engine_name:
+                return
+
+        options = template_config.setdefault("OPTIONS", {})
+
+        self.configure_loaders(options)
+        self.configure_builtins(options)
+
+        # Force re-evaluation of settings.TEMPLATES because EngineHandler caches it.
+        with suppress(AttributeError):
+            del django.template.engines.templates
+            django.template.engines._engines = {}
+
+        self._configured = True
+
+    def configure_loaders(self, options: dict[str, Any]) -> None:
+        loaders = options.setdefault("loaders", [])  # type: ignore[attr-defined]
+
+        # find the inner-most loaders, which is an iterable of only strings
+        while not all(isinstance(loader, str) for loader in loaders):
+            for loader in loaders:
+                # if we've found a list or tuple, we aren't yet in the inner-most loaders
+                if isinstance(loader, list | tuple):
+                    # reassign `loaders` variable to force the while loop restart
+                    loaders = loader
+
+        # if django-bird's loader is the first, we good
+        loaders_already_configured = (
+            len(loaders) > 0 and "django_bird.loader.BirdLoader" == loaders[0]
+        )
+
+        if not loaders_already_configured:
+            loaders.insert(0, "django_bird.loader.BirdLoader")
+
+    def configure_builtins(self, options: dict[str, Any]) -> None:
+        builtins = options.setdefault("builtins", [])  # type: ignore[attr-defined]
+
+        builtins_already_configured = "django_bird.templatetags.django_bird" in builtins
+
+        if not builtins_already_configured:
+            builtins.append("django_bird.templatetags.django_bird")
 
 
 app_settings = AppSettings()
--- tests/test_conf.py
+++ tests/test_conf.py
@@ -1,11 +1,102 @@
 from __future__ import annotations
 
 import pytest
+from django.conf import settings
+from django.test import SimpleTestCase
+from django.test import override_settings
 
+from django_bird.conf import AppSettings
+from django_bird.conf import TemplateConfigurator
 from django_bird.conf import app_settings
 
 
 def test_app_settings():
-    # stub test until `bird` requires custom app settings
-    with pytest.raises(AttributeError):
-        assert app_settings.foo
+    assert app_settings.ENABLE_AUTO_CONFIG is True
+
+
+@override_settings(
+    DJANGO_BIRD={
+        "ENABLE_AUTO_CONFIG": False,
+    }
+)
+class TestAutoConfigure(SimpleTestCase):
+    def test_app_settings(self):
+        assert app_settings.ENABLE_AUTO_CONFIG is False
+
+
+class TestTemplateConfigurator:
+    DJANGO_BIRD_BUILTINS = "django_bird.templatetags.django_bird"
+    DJANGO_BIRD_LOADER = "django_bird.loader.BirdLoader"
+
+    @pytest.fixture
+    def configurator(self):
+        return TemplateConfigurator(app_settings)
+
+    @pytest.fixture
+    def template_options(self):
+        return settings.TEMPLATES[0]["OPTIONS"]
+
+    @pytest.fixture(autouse=True)
+    def reset_settings(self, template_options):
+        assert self.DJANGO_BIRD_LOADER in template_options["loaders"]
+        assert self.DJANGO_BIRD_BUILTINS in template_options["builtins"]
+
+        with override_settings(
+            TEMPLATES=[
+                settings.TEMPLATES[0]
+                | {
+                    **settings.TEMPLATES[0],
+                    "OPTIONS": {
+                        "loaders": [
+                            loader
+                            for loader in template_options["loaders"]
+                            if loader != self.DJANGO_BIRD_LOADER
+                        ],
+                        "builtins": [
+                            builtin
+                            for builtin in template_options["builtins"]
+                            if builtin != self.DJANGO_BIRD_BUILTINS
+                        ],
+                    },
+                }
+            ]
+        ):
+            options = settings.TEMPLATES[0]["OPTIONS"]
+
+            assert self.DJANGO_BIRD_LOADER not in options["loaders"]
+            assert self.DJANGO_BIRD_BUILTINS not in options["builtins"]
+
+            yield
+
+    def test_autoconfigure(self, configurator, template_options):
+        configurator.autoconfigure()
+
+        assert self.DJANGO_BIRD_LOADER in template_options["loaders"]
+        assert self.DJANGO_BIRD_BUILTINS in template_options["builtins"]
+
+    def test_configure_loaders(self, configurator, template_options):
+        configurator.configure_loaders(template_options)
+
+        assert self.DJANGO_BIRD_LOADER in template_options["loaders"]
+
+    def test_configure_builtins(self, configurator, template_options):
+        configurator.configure_builtins(template_options)
+
+        assert self.DJANGO_BIRD_BUILTINS in template_options["builtins"]
+
+    def test_configured(self, configurator):
+        assert configurator._configured is False
+
+        configurator.autoconfigure()
+
+        assert configurator._configured is True
+
+    def test_app_settings(self, configurator):
+        app_settings = AppSettings()
+        app_settings._template_configurator = configurator
+
+        assert app_settings._template_configurator._configured is False
+
+        configurator.autoconfigure()
+
+        assert app_settings._template_configurator._configured is True
