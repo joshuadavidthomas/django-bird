d35d517f4e4e0565f3f67800c7aec73c1b9ef3bd
Josh Thomas <josh@joshthomas.dev>
2024-11-27 16:27:10 -0500

    reorganize tags to dedicated directory (#48)
    * reorganize tags to dedicated directory
    
    * move tests

---
+++ src/django_bird/templatetags/tags/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .bird import do_bird
+from .slot import do_slot
+
+__all__ = [
+    "do_bird",
+    "do_slot",
+]
---
+++ src/django_bird/templatetags/tags/slot.py
@@ -0,0 +1,61 @@
+# pyright: reportAny=false
+from __future__ import annotations
+
+from typing import cast
+
+from django import template
+from django.template.base import NodeList
+from django.template.base import Parser
+from django.template.base import Token
+from django.template.context import Context
+from django.utils.safestring import SafeString
+from django.utils.safestring import mark_safe
+
+from django_bird._typing import override
+
+
+def do_slot(parser: Parser, token: Token) -> SlotNode:
+    bits = token.split_contents()
+    name = parse_slot_name(bits)
+    nodelist = parser.parse(("endbird:slot",))
+    parser.delete_first_token()
+    return SlotNode(name, nodelist)
+
+
+def parse_slot_name(tag_args: list[str]) -> str:
+    if len(tag_args) == 1:
+        return "default"
+    elif len(tag_args) == 2:
+        name = tag_args[1]
+        if name.startswith("name="):
+            name = name.split("=")[1]
+        else:
+            name = name
+        return name.strip("'\"")
+    else:
+        raise template.TemplateSyntaxError(
+            "slot tag requires either no arguments, one argument, or 'name=\"slot_name\"'"
+        )
+
+
+class SlotNode(template.Node):
+    def __init__(self, name: str, nodelist: NodeList):
+        self.name = name
+        self.nodelist = nodelist
+
+    @override
+    def render(self, context: Context) -> SafeString:
+        default_content: str = self.nodelist.render(context)
+        slots = context.get("slots")
+
+        if not slots or not isinstance(slots, dict):
+            slot_content = default_content
+        else:
+            slots_dict = cast(dict[str, str], slots)
+            slot_content = slots_dict.get(self.name, default_content)
+
+        # Recursively process the slot content
+        t = template.Template(slot_content)
+        content = t.render(context)
+
+        return mark_safe(content)
