aa141563eb95ffc61ed3ecb72f84a635590cf1fd
Josh <josh@joshthomas.dev>
2024-12-02 23:35:32 -0600

    wip

---
+++ src/django_bird/components.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+from django.template.backends.django import Template
+from django.template.base import NodeList
+from django.template.loader import select_template
+from django.utils.safestring import mark_safe
+
+from .cache import LRUCache
+from .templates import get_template_names
+
+
+@dataclass(frozen=True, slots=True)
+class Component:
+    name: str
+    template: Template
+
+    def render(self, context: dict[str, Any]):
+        return mark_safe(self.template.render(context))
+
+    @property
+    def nodelist(self) -> NodeList:
+        return self.template.template.nodelist
+
+    @classmethod
+    def from_name(cls, name: str):
+        template_names = get_template_names(name)
+        template = select_template(template_names)
+        return cls(name=name, template=template)
+
+
+class Registry:
+    def __init__(self, maxsize: int = 100):
+        self._cache: LRUCache[str, Component] = LRUCache(maxsize=maxsize)
+
+    def get_component(self, name: str) -> Component:
+        try:
+            return self._cache[name]
+        except KeyError:
+            component = Component.from_name(name)
+            self._cache[name] = component
+            return component
+
+
+registry = Registry()
