7e781b5a6f9bbdef768cacf61038821bc3750dcb
Josh <josh@joshthomas.dev>
2025-01-31 10:54:35 -0600

    wip

--- src/django_bird/urls.py
+++ src/django_bird/urls.py
@@ -4,9 +4,13 @@ from django.urls import path
 
 from .apps import DjangoBirdAppConfig
 from .views import asset_view
+from .views import component_detail
+from .views import component_list
 
 app_name = DjangoBirdAppConfig.label
 
 urlpatterns = [
     path("assets/<str:component_name>/<str:asset_filename>", asset_view, name="asset"),
+    path("components/", component_list, name="component-list"),
+    path("components/<str:component_name>/", component_detail, name="component-detail"),
 ]
--- src/django_bird/views.py
+++ src/django_bird/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.http import FileResponse
 from django.http import Http404
 from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import render
 from django.template.exceptions import TemplateDoesNotExist
 
 from .components import components
@@ -39,3 +41,32 @@ def asset_view(request: HttpRequest, component_name: str, asset_filename: str):
     content = asset.path.read_bytes()
 
     return FileResponse(BytesIO(content), content_type=asset.type.content_type)
+
+
+def component_list(request: HttpRequest) -> HttpResponse:
+    components.discover_components()
+    return render(
+        request,
+        "component_list.html",
+        {
+            "components": sorted(
+                list(components._components.values()),
+                key=lambda component: component.name,
+            )
+        },
+    )
+
+
+def component_detail(request: HttpRequest, component_name: str) -> HttpResponse:
+    components.discover_components()
+    return render(
+        request,
+        "component_detail.html",
+        {
+            "components": sorted(
+                list(components._components.values()),
+                key=lambda component: component.name,
+            ),
+            "component": components.get_component(component_name),
+        },
+    )
