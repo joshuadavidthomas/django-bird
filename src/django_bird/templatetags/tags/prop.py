38160c4e3a78e36a78c28ab72af4e7b75c445c4a
Josh <josh@joshthomas.dev>
2025-01-31 12:36:42 -0600

    refactor and clean up prop templatetag

--- src/django_bird/templatetags/tags/prop.py
+++ src/django_bird/templatetags/tags/prop.py
@@ -1,6 +1,8 @@
 # pyright: reportAny=false
 from __future__ import annotations
 
+from typing import final
+
 from django import template
 from django.template.base import Parser
 from django.template.base import Token
@@ -12,29 +14,24 @@ from django_bird._typing import override
 TAG = "bird:prop"
 
 
-def do_prop(parser: Parser, token: Token) -> PropNode:
-    bits = token.split_contents()
-    name, default = parse_prop_name(bits)
-    attrs = parse_attrs(bits)
-    return PropNode(name, default, attrs)
-
-
-def parse_prop_name(bits: TagBits) -> tuple[str, str | None]:
-    if len(bits) <= 1:
+def do_prop(_parser: Parser, token: Token) -> PropNode:
+    _tag, *bits = token.split_contents()
+    if not bits:
         msg = f"{TAG} tag requires at least one argument"
         raise template.TemplateSyntaxError(msg)
 
+    prop = bits.pop(0)
+
     try:
-        name, default = bits[1].split("=")
-        return name, default.strip("'\"")
+        name, default = prop.split("=")
     except ValueError:
-        return bits[1], None
+        name = prop
+        default = None
+
+    return PropNode(name, default, bits)
 
 
-def parse_attrs(bits: TagBits) -> TagBits:
-    return bits[2:]
-
-
+@final
 class PropNode(template.Node):
     def __init__(self, name: str, default: str | None, attrs: TagBits):
         self.name = name
--- tests/templatetags/test_prop.py
+++ tests/templatetags/test_prop.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
 import pytest
-from django.template.exceptions import TemplateSyntaxError
+from django.template.base import Parser
+from django.template.base import Token
+from django.template.base import TokenType
 
-from django_bird.templatetags.tags.prop import parse_prop_name
+from django_bird.templatetags.tags.prop import TAG
+from django_bird.templatetags.tags.prop import do_prop
 
 
 @pytest.mark.parametrize(
-    "bits,expected",
+    "prop,expected",
     [
-        (["bird:prop", "id"], ("id", None)),
-        (["bird:prop", "class='btn'", "foo"], ("class", "btn")),
+        ("id", ("id", None)),
+        ("class='btn'", ("class", "'btn'")),
     ],
 )
-def test_parse_prop_name(bits, expected):
-    assert parse_prop_name(bits) == expected
+def test_name_do_prop(prop, expected):
+    start_token = Token(TokenType.BLOCK, f"{TAG} {prop}")
 
+    node = do_prop(Parser([]), start_token)
 
-def test_parse_prop_name_no_args():
-    with pytest.raises(TemplateSyntaxError):
-        parse_prop_name([])
+    assert node.name == expected[0]
+    assert node.default == expected[1]
