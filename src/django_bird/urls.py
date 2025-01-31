7ef27db82810c15f03220498ed62d06e498f0ab1
Josh <josh@joshthomas.dev>
2025-01-31 10:54:35 -0600

    wip

--- src/django_bird/urls.py
+++ src/django_bird/urls.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+from django.urls import path
 from .apps import DjangoBirdAppConfig
+from .views import component_detail
+from .views import component_list
 
 app_name = DjangoBirdAppConfig.label
 
-urlpatterns = []  # type: ignore[var-annotated]
+urlpatterns = [
+    path("components/", component_list, name="component-list"),
+    path("components/<str:component_name>/", component_detail, name="component-detail"),
+]
--- src/django_bird/views.py
+++ src/django_bird/views.py
@@ -1,1 +1,50 @@
 from __future__ import annotations
+
+import warnings
+from io import BytesIO
+
+from django.conf import settings
+from django.http import FileResponse
+from django.http import Http404
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
+
+from .components import components
+
+
+
+
+from .components import components
+
+
+
+
+def component_list(request: HttpRequest) -> HttpResponse:
+    components.discover_components()
+    return render(
+        request,
+        "component_list.html",
+        {
+            "components": sorted(
+                list(components._components.values()),
+                key=lambda component: component.name,
+            )
+        },
+    )
+
+
+def component_detail(request: HttpRequest, component_name: str) -> HttpResponse:
+    components.discover_components()
+    return render(
+        request,
+        "component_detail.html",
+        {
+            "components": sorted(
+                list(components._components.values()),
+                key=lambda component: component.name,
+            ),
+            "component": components.get_component(component_name),
+        },
+    )
