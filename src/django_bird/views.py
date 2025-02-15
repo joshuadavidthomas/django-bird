e03df9f98e5a27b00867448682ce692ae29e41c6
Josh <josh@joshthomas.dev>
2025-02-14 23:54:48 -0600

    fix

--- src/django_bird/views.py
+++ src/django_bird/views.py
@@ -1,24 +1,10 @@
 from __future__ import annotations
 
-import warnings
-from io import BytesIO
-
-from django.conf import settings
-from django.http import FileResponse
-from django.http import Http404
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.template.exceptions import TemplateDoesNotExist
 
 from .components import components
-
-
-
-
-from .components import components
-
-
 
 
 def component_list(request: HttpRequest) -> HttpResponse:
