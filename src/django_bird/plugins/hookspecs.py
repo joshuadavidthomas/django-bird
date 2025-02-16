87dae23490e6b0c66c4f4cd0b51564cb2802edaf
Josh Thomas <josh@joshthomas.dev>
2025-02-16 13:48:24 -0600

    Add documentation for plugins (#188)
    * Add documentation for plugins
    
    Add enhanced docstring for collect_component_assets hook and create plugin documentation
    
    add documentation for plugins
    
    * adjust imports

--- src/django_bird/plugins/hookspecs.py
+++ src/django_bird/plugins/hookspecs.py
@@ -12,4 +12,9 @@ hookspec = HookspecMarker("django_bird")
 
 @hookspec
 def collect_component_assets(template_path: Path) -> Iterable[Asset]:
-    """Collect all assets associated with a component."""
+    """Collect all assets associated with a component.
+
+    This hook is called for each component template to gather its associated static assets.
+    Implementations should scan for and return any CSS, JavaScript or other static files
+    that belong to the component and return a list/set/other iterable of `Asset` objects.
+    """
